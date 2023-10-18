236c3c14f79634aa78104aec87a2de67c74ad944
THIAGO CORREA BRANDAO <202301135265@alunos.ibmec.edu.br>
2023-10-18 16:40:15 -0300

    link generico

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,11 +1,28 @@
-import LinkSegundo from "../components/LinkSegundo";
+import LinkGenerico from "../components/Linkgenerico";
 
 const Primeiro = () => {
+    
+    const dados  = [
+        ["https://g1.globo.com/" , "G1"],
+        ["https://vasco.com.br/", "Vasco"],
+        ["https://botafogo.com.br/", "Botafogo"]
+    ];
+
+    const links = dados.map((ele) => (
+        <LinkGenerico
+           key = {ele[1]}
+           texto= {ele[1]}
+           endereco= {ele[0]}
+         />
+    ))
+    
+    
+    
     return (<div>
-        <h5>Mini titulo</h5>
-        <p>Meu texto</p>
-        <LinkSegundo/>
-        </div>);
+             <h5>Mini titulo</h5>
+             <p>Meu texto</p>
+             {links}
+            </div>);
 }
 
 export default Primeiro
