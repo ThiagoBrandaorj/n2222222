af060e038c533ca845b56b0e4dab1191f74208c4
THIAGO CORREA BRANDAO <202301135265@alunos.ibmec.edu.br>
2023-10-18 17:10:32 -0300

    links e style

---
+++ src/components/Linkgenerico/style.js
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+const MeuA = styled.a`
+    margin: 2em;
+
+`;
+
+export {MeuA};
--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,11 +1,14 @@
-import LinkGenerico from "../components/Linkgenerico";
+import LinkGenerico from "../components/Linkgenerico/Linkgenerico";
 
 const Primeiro = () => {
     
     const dados  = [
         ["https://g1.globo.com/" , "G1"],
         ["https://vasco.com.br/", "Vasco"],
-        ["https://botafogo.com.br/", "Botafogo"]
+        ["https://botafogo.com.br/", "Botafogo"],
+        ["https://github.com/ThiagoBrandaorj", "meu_Github"],
+        ["https://pt.wikipedia.org/wiki/Lionel_Messi", "Messi"],
+        ["https://www.gov.br/receitafederal/pt-br", "Receita_Federal"]
     ];
 
     const links = dados.map((ele) => (
