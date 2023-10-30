112bfef7c3ddfa1cd802dab50c69dfbaa2f2a762
Thiago <thiagocbrandao2003@gmail.com>
2023-10-30 14:33:47 -0300

    ???

--- src/pages/Listagem/Listagem.jsx
+++ src/pages/Listagem/Listagem.jsx
@@ -1,4 +1,4 @@
-import Cartao from "../../components/Cartao/Cartao";
+import Cartao from "../../components/Cartao/cartao.jsx";
 import { ContainerListagem, TituloListagem } from "./style";
 
 const dados = [
