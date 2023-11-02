06e0727b81555fb5f78c3c34a7d207d29f449a34
Thiago <thiagocbrandao2003@gmail.com>
2023-11-02 13:34:14 -0300

    filter add

--- src/pages/Listagem/Listagem.jsx
+++ src/pages/Listagem/Listagem.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import Cartao from "../../components/Cartao/Cartao.jsx";
 import { ContainerListagem, TituloListagem } from "./style";
 
-const dados = [
+const predados = [
     {
         'titulo': 'ananais',
         'letra': 'z'
@@ -24,7 +25,29 @@ const dados = [
     }
 ]
 
-const cartoes = dados.map(
+const Listagem = () => {
+    const [dados, setDados] = useState(predados);
+
+    const filtar = (entrada) => {
+        const resposta = predados.filter(
+            (e) => {
+                const titulo = e.titulo.toLowerCase().includes(entrada)
+                const letra = e.letra.toLowerCase().includes(entrada)
+                return titulo || letra;
+            }
+        )
+        setDados(resposta);
+    }
+
+    return(
+    <>
+    <TituloListagem>Listagem</TituloListagem>
+    <input 
+       type="text"
+       onChange={(e) => filtar(e.target.value)}
+       />
+    <ContainerListagem>
+        {dados.map(
     (el, i) => (
         <Cartao
             key={i}
@@ -32,16 +55,10 @@ const cartoes = dados.map(
             letra={el.letra}
         />
     )
-)
-
-const Listagem = () => (
-    <>
-    <TituloListagem>Listagem</TituloListagem>
-    <ContainerListagem>
-        {cartoes}
+)}
     </ContainerListagem>
-    <p>isso</p>
     </>
 );
+}
 
 export default Listagem;
